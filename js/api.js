a6e4baf2879af0daeb5ce3abe90717bd231a32d5
marcomarciano <info@marcomarciano.com>
2016-11-27 15:59:29 +0100

    Add files via upload

---
+++ js/api.js
@@ -0,0 +1,22 @@
+$.ajax({
+
+				url:"http://graph.facebook.com/me",
+				method:"GET",
+				appId      : '142748222869667',
+      			xfbml      : true,
+      			version    : 'v2.8'
+				
+				data:{
+
+
+
+
+				},
+				success: handleResults,
+
+			});
+
+function handleResults(response_body) {
+
+	console.log(response_body);
+}
